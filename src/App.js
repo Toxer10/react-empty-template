29a4933dcb1e86ed47ef82ef7f0ff35c85f17fe5
ediv <ediv@opinov8.com>
2021-04-24 17:03:09 +0300

    react template: initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <h1>Empty react proj</h1>
+    </div>
+  )
+}
+
+export default App
